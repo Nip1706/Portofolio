bb102987eb584be4fbff5ff1799fe59ad37a9a7f
Nip1706 <payagalage.nipuna@gmail.com>
2023-06-02 22:01:34 +0200

    portofolio projet page

---
+++ projets.js
@@ -0,0 +1,28 @@
+const carousel = document.querySelector('.project-carousel');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1, // Affiche un seul projet à la fois
+  spaceBetween: 20, // Espacement entre les projets
+  navigation: {
+    nextEl: '.next-button',
+    prevEl: '.prev-button',
+  },
+});
+
+prevButton.addEventListener('click', () => {
+  carousel.scrollBy({
+    left: -320,
+    behavior: 'smooth'
+  });
+});
+
+nextButton.addEventListener('click', () => {
+  carousel.scrollBy({
+    left: 320,
+    behavior: 'smooth'
+  });
+});
+
+
